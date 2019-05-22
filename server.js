d56a2fc482e1fb9dbb3e2a35d3e08280b08f97e1
Kodjo Edem <kodjosama@gmail.com>
2019-05-22 12:01:01 +0000

    Setup Heroku procfile and configure an express server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const path = require('path')
+const serveStatic = require('serve-static')
+
+const app = express()
+
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+app.listen(process.env.PORT || 5000);
